d96364d07d949b6e8669523756ab7297ced967a0
vetrivel <vetrivel.kce@gmail.com>
2022-08-06 17:46:46 +0530

    ucone attendance

---
+++ EmpAttendence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpPresentAbsent
+{
+    internal class EmpAttendence
+    {
+        public static void EmpAttd()
+        {
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int Check = random.Next(0, 2);
+            Console.WriteLine(Check);
+            if (Check == IS_PRESENT)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace EmpPresentAbsent
+{
+    class Program
+    {
+        public static void Main()
+        {
+            EmpAttendence.EmpAttd();
+        }
+    }
+}
