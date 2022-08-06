8265dcad6a12158d12c186e5d801ef6db57d5965
vetrivel <vetrivel.kce@gmail.com>
2022-08-06 17:32:36 +0530

    welcome to employee wage computation program

---
+++ Program (1).cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace EmployeeWage
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Wage Computation Program");
+        }
+    }
+}
